6eb02f7cf27b1fb99ed4c7455d4a0243f9daf0aa
Sergei Poluboiarinov <sihtmark@icloud.com>
2023-02-08 16:46:24 +0300

    Initial Commit

---
+++ FirebaseLearning/FirebaseLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLearningApp.swift
+//  FirebaseLearning
+//
+//  Created by Sergei Poluboiarinov on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
